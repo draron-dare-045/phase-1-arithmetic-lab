608aac2cc5f24ecea2e53869af89e62b84f964b4
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-03-18 14:40:34 +0300

    4 passing

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = (num1*num2);
+
+const random = Math.floor(Math.random() * 10);
+
+let num3 = 10;
+let num4 = 6;
+const mod = (num3%num4);
+
+let num5 = 20;
+let num6 =15;
+const max = Math.max(num2,num3,num4,num5,num6);
